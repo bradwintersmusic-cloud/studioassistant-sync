3f331e0e35756550a7489e5f769fd45132a60766
Brad Winters <bradwinters.music@gmail.com>
2026-06-30 08:38:29 -0500

    add tools to nav

--- scripts/studioassistant-syncv5.js
+++ scripts/studioassistant-syncv5.js
@@ -592,6 +592,16 @@ function buildHTML(sessions, generatedAt, todayKey, tomorrowKey) {
       </span>
       <span class="belmont-nav-link-label">Policies</span>
     </a>
+
+    <!-- Tools -->
+    <a class="belmont-nav-link" data-page="tools" href="https://bradwintersmusic-cloud.github.io/studio-tools/">
+        <span class="belmont-nav-link-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/>
+          </svg>
+        </span>
+        <span class="belmont-nav-link-label">Studio Tools</span>
+      </a>
  
   </nav>
 </div>
